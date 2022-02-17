71e7ceba80614c2953476ebb5f1cd59b2bdddecb
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2022-02-17 13:14:41 +0530

    refactor bulk create job

--- app/workers/bulk_post_job.rb
+++ app/workers/bulk_post_job.rb
@@ -1,18 +1,28 @@
 class BulkPostJob
   include Sidekiq::Job
+  sidekiq_options tags: ['tag1', '🥇']
 
-  def perform(count)
-    # count&.times do |i|
-    #   puts "Posting #{i}"
-    #   Post.create!(name: Faker::Name.name, title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
-    #   puts "Posted #{i}"
-    # end
+  # visit https://github.com/mperham/sidekiq/wiki/Advanced-Options#workers to see available options
 
-    puts "Starting bulk creation @ #{Time.now}"
+  def perform(type = 's', count = 10)
+    sleep(2.seconds)
+    puts "** Starting bulk creation @ #{Time.now} **"
+    type == 's' ? single_insert(count) : multi_insert(count)
+    puts "** Finished bulk creation @ #{Time.now} **"
+  end
+
+  private
+
+  def single_insert(count)
+    puts "#{'🚨' * 4} single_insert  #{'🚨' * 4}"
+    count.times { Post.create!(name: Faker::Name.name, title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph) }
+  end
+
+  def multi_insert(count)
+    puts "#{'Ⓜ️' * 4} multi_insert  #{'Ⓜ️' * 4}"
     posts = (1..count).map do
       { name: Faker::Name.name, title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph }
     end
     Post.create!(posts)
-    puts "Finished bulk creation @ #{Time.now}"
   end
 end
